c0c6593a927e2db3a3667965384138a6a0c4353c
muharremmandaci <muharremmandaci@gmail.com>
2016-10-29 13:26:36 +0300

    Add files via upload

---
+++ is_prime.c
@@ -0,0 +1,48 @@
+/* Libraries */
+#include <stdio.h>
+#include <conio.h>
+
+/* Function Prototypes */
+int is_prime(int );
+
+/* Main Function */ 
+int main()
+{
+	/*  Variables */
+	int limit = 1000;
+	int toplam = 0;
+
+	/* counter */
+	int i;	
+	for (i = 2; i < limit; i++)
+	{
+		if (is_prime(i))
+		{
+			toplam += i;
+		}
+	}
+
+	printf("%d\n", toplam);
+
+	return 0;
+}
+
+/* Function Definitions */
+int is_prime(int number)
+{
+	if (number == 2)
+	{
+		return 1;
+	}
+
+	/* counter */
+	int i;
+	/* control is_prime */
+	for (i = 3; i < number; i++)
+	{
+		if(number % i == 0){
+			return 0;//not prime
+		}
+	}
+	return 1;//prime
+}
